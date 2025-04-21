9c19183050f39397ff2292873585a4298bd54de3
Gabriel Rufino <contato@gabrielrufino.com>
2025-04-21 09:23:04 -0300

    chore: update eslint config

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,13 +1,13 @@
-import { faker } from '@faker-js/faker';
-import { describe, expect, it } from 'vitest';
+import { faker } from '@faker-js/faker'
+import { describe, expect, it } from 'vitest'
 
-import { add } from '.';
+import { add } from '.'
 
 describe('add', () => {
   it('should add two numbers', () => {
-    const a = faker.number.int();
-    const b = faker.number.int();
+    const a = faker.number.int()
+    const b = faker.number.int()
 
-    expect(add(a, b)).toBe(a + b);
-  });
-});
+    expect(add(a, b)).toBe(a + b)
+  })
+})
